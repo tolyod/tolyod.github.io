b0cd6fcd84d4d2c7b1071b58b6d8e1d6068dcacc
Anatoliy Poloz <anatoliy.poloz@gmail.com>
2019-03-26 20:49:01 +0300

    react-mobx-crud-test-task added

---
+++ react-mobx-crud-test-task/precache-manifest.6004f2b7d2b280b5408e19bb3fa697a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aa38fee2ab503d1379f",
+    "url": "/react-mobx-crud-test-task/static/js/runtime~main.7ad4c432.js"
+  },
+  {
+    "revision": "e057678d1e23c42f85ca",
+    "url": "/react-mobx-crud-test-task/static/js/main.f1841547.chunk.js"
+  },
+  {
+    "revision": "8d509864b23394b927db",
+    "url": "/react-mobx-crud-test-task/static/js/2.8cc39178.chunk.js"
+  },
+  {
+    "revision": "e057678d1e23c42f85ca",
+    "url": "/react-mobx-crud-test-task/static/css/main.5d06b741.chunk.css"
+  },
+  {
+    "revision": "8d509864b23394b927db",
+    "url": "/react-mobx-crud-test-task/static/css/2.822700a0.chunk.css"
+  },
+  {
+    "revision": "154d2204484aaf9944f6e9c1d2d5a4a3",
+    "url": "/react-mobx-crud-test-task/index.html"
+  }
+];
